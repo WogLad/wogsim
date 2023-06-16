af42e85a7e0a7c59dca19cb151b2ea9baa867bfa
WogLad <aaronbusiness2005@gmail.com>
2023-06-16 14:55:09 +0400

    Added point and drag movement controls to the world viewer

--- js/UI.js
+++ js/UI.js
@@ -12,3 +12,55 @@ canvas.addEventListener("click", (e) => {
 document.getElementById("togglePauseButton").addEventListener("click", (e) => {
     PAUSED = !PAUSED;
 });
+// TODO: Add movement controls that can adjust the camera offset
+var isMouseDown = false;
+var initMousePos = { x: 0, y: 0 };
+var mouseTilePos = { x: 0, y: 0 };
+canvas.onpointermove = (e) => {
+    var rect = e.target.getBoundingClientRect();
+    mousePos.x = e.clientX - rect.left; //x position within the element.
+    mousePos.y = e.clientY - rect.top; //y position within the element.
+    if (Math.floor(mousePos.x / TILE_SIZE) != mouseTilePos.x || Math.floor(mousePos.y / TILE_SIZE) != mouseTilePos.y) {
+        // Mouse Tile Pos Has Changed
+        mouseTilePos.x = Math.floor(mousePos.x / TILE_SIZE) + CAMERA_OFFSET.x;
+        mouseTilePos.y = Math.floor(mousePos.y / TILE_SIZE) + CAMERA_OFFSET.y;
+    }
+    // SWIPE GESTURES
+    if (isMouseDown) {
+        // (DONE): Fix the Y-axis issue
+        // (DONE): Fix the mobile touch issue
+        if (initMousePos.x != 0 && initMousePos.y != 0) {
+            var pointerDragOffset = Vector2(mouseTilePos.x - initMousePos.x, mouseTilePos.y - initMousePos.y);
+            if (pointerDragOffset.x < 0 && CAMERA_OFFSET.x + 1 < X_TILES) {
+                setCameraOffset(CAMERA_OFFSET.x + 1, CAMERA_OFFSET.y);
+            }
+            else if (pointerDragOffset.x > 0 && CAMERA_OFFSET.x - 1 >= 0) {
+                setCameraOffset(CAMERA_OFFSET.x - 1, CAMERA_OFFSET.y);
+            }
+            if (pointerDragOffset.y < 0 && CAMERA_OFFSET.y + 1 < Y_TILES) {
+                setCameraOffset(CAMERA_OFFSET.x, CAMERA_OFFSET.y + 1);
+            }
+            else if (pointerDragOffset.y > 0 && CAMERA_OFFSET.y - 1 >= 0) {
+                setCameraOffset(CAMERA_OFFSET.x, CAMERA_OFFSET.y - 1);
+            }
+        }
+    }
+};
+canvas.addEventListener("pointerdown", (e) => {
+    isMouseDown = true;
+    initMousePos.x = mouseTilePos.x;
+    initMousePos.y = mouseTilePos.y;
+    var rect = e.target.getBoundingClientRect();
+    mousePos.x = e.clientX - rect.left; //x position within the element.
+    mousePos.y = e.clientY - rect.top; //y position within the element.
+    if (Math.floor(mousePos.x / TILE_SIZE) != mouseTilePos.x || Math.floor(mousePos.y / TILE_SIZE) != mouseTilePos.y) {
+        // Mouse Tile Pos Has Changed
+        mouseTilePos.x = Math.floor(mousePos.x / TILE_SIZE) + CAMERA_OFFSET.x;
+        mouseTilePos.y = Math.floor(mousePos.y / TILE_SIZE) + CAMERA_OFFSET.y;
+    }
+});
+canvas.addEventListener("pointerup", (e) => {
+    isMouseDown = false;
+    initMousePos.x = 0;
+    initMousePos.y = 0;
+});
